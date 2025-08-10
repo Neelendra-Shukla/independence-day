8d95b753096d81cdcbce02488d3d46aeccfaead3
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-08-10 15:02:43 +0530

    update

--- my-project/src/Components/Header.jsx
+++ my-project/src/Components/Header.jsx
@@ -4,9 +4,9 @@ import NavBar from './NavBar';
 
 const Header = () => {
     return <>
-    <div className=''>
+    <div className='head'>
         <NavBar />
-        <div className='flex flex-row justify-center items-center m-19 max-w-10xl gap-50 '>
+        <div className='flex flex-row justify-center items-center m-19 max-w-10xl gap-50  '>
             <div>
                 <h1 className='text-5xl mb-4 font-bold text-orange-500'>Digital Freedom Gallery </h1>
                 <p className='text-3xl mb-19 font-light text-white'> Code a Vision. Design the Nation</p>
